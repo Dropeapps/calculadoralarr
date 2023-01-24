31d0b4c3a25621f403512484869ba5c727fb71f6
Dropeapps <123127131+Dropeapps@users.noreply.github.com>
2023-01-24 19:32:27 -0300

    Update calculatorlar.py

--- calculatorlar.py
+++ calculatorlar.py
@@ -1,12 +1,12 @@
 import streamlit as st
-
+st.tittle('Calculadora de Peso')
 n1=st.number_input('Peso: ')
 n2=st.number_input('Qtd Solicitada: ')
 n3=(n1*0.10+n1)*n2
 st.write(f'O peso total é de {n3} ')
 
-
+st.tittle('Calculadora de Horas')
 n1 = st.number_input("Quantidade solicitada: ")
 n2 = st.number_input("Peças por hora: ")
 n3 = n1/n2
-st.write(f'A máquina rodará durante {n3} horas')
+st.write(f'A máquina rodará durante {n3:.2f} horas')
