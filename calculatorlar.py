0882ceb2b8d3bc1a3754adf98e8726af85230aac
Dropeapps <123127131+Dropeapps@users.noreply.github.com>
2023-01-25 20:33:13 -0300

    Update calculatorlar.py

--- calculatorlar.py
+++ calculatorlar.py
@@ -8,5 +8,5 @@ st.write(f'O peso total é de {n3:.2f} ')
 st.title('Calculadora de Horas')
 n1 = st.number_input("Quantidade solicitada: ")
 n2 = st.number_input("Peças por hora: ")
-n3 = n1/n2
+n3 = (n1/n2)*1
 st.write(f'A máquina rodará durante {n3:.2f} horas')
